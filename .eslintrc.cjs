97403240e9aec6e8d2f287b6c0e508c111fb5d59
zaripych <zaripov.rinat@gmail.com>
2022-06-26 00:42:44 +1000

    chore: some auto-generated code and package json changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,11 @@
+// Hello fellow developer 👋
+//
+// I feel sad to be a shameful plug of an extra config.
+//
+// I'm essential for your development environment and IDE to recognize that
+// eslint exists and start picking up the only rules that you only
+// ever need. Feel free to override them standard eslint ways!
 module.exports = {
   root: true,
-  extends: ['./packages/build-tools/ts/configs/eslint/eslint-root.cjs'],
+  extends: ['./node_modules/@build-tools/ts/configs/eslint/eslint-root.cjs'],
 };
