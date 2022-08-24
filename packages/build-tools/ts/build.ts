7b8f94e3ce07a950d279691c50eea1f05ea9eeb2
Rinat Zaripov <zaripov.rinat@gmail.com>
2022-08-24 22:28:54 +1000

    Revert "fix(ts-types): include @types/jest and @types/node"
    This reverts commit 5339c3ab0365fd9ed6a95a42039e09b1c44f7ce0.

--- packages/build-tools/ts/build.ts
+++ packages/build-tools/ts/build.ts
@@ -1,14 +1,6 @@
-import { basename, join } from 'path';
-
 import { buildForNode, copy, pipeline } from './src';
 import { buildEslintConfigHelpers } from './src/eslint/eslintConfigHelpers.build.js';
 import { buildJestConfigHelpers } from './src/jest/jestConfigHelpers.build.js';
-import { findTypeDependencies } from './src/utils/findTypeDependencies';
-
-const typeDependencies = await findTypeDependencies([
-  '@types/node',
-  '@types/jest',
-]);
 
 await pipeline(
   buildForNode({
@@ -20,12 +12,5 @@ await pipeline(
   copy({
     include: ['configs/**/*'],
     destination: './dist/',
-  }),
-  ...typeDependencies.map((source) =>
-    copy({
-      include: ['/**/*'],
-      destination: join('./dist/@types', basename(source)),
-      source,
-    })
-  )
+  })
 );
