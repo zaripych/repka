3d6ceec160cc564de43c393427b84d49942aae48
Rinat Zaripov <zaripov.rinat@gmail.com>
2022-09-03 10:00:59 +1000

    chore: improve local lint-staged config to include generated files

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,9 @@
 import config from './packages/build-tools/ts/configs/lint-staged/lintStaged.mjs';
 
-export default config;
+export default {
+  ...config,
+  ['packages/build-tools/ts/**/*']: [
+    () => `repka --cwd packages/build-tools/ts build:node`,
+    'git add -- :(glob)packages/build-tools/ts/**/*.gen.*',
+  ],
+};
