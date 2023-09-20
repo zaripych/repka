45c740c8b79ab6ea7c3ef4204f05394952794829
Rinat Zaripov <zaripov.rinat@gmail.com>
2023-09-20 13:10:28 +1000

    fix: npx init bug and add documentation

--- packages/build-tools/ts/src/utils/findDevDependency.ts
+++ packages/build-tools/ts/src/utils/findDevDependency.ts
@@ -10,7 +10,7 @@ export { repositoryRootPath } from './repositoryRootPath';
 
 async function lookup(opts: { path: string; lookupPackageName: string }) {
   return await upwardDirectorySearch({
-    start: moduleRootDirectory(),
+    start: opts.path,
     appendPath: join('node_modules', opts.lookupPackageName),
     test: isDirectory,
   });
@@ -20,11 +20,14 @@ async function lookup(opts: { path: string; lookupPackageName: string }) {
  * Lookup location for devDependencies of "@repka-kit/ts" - this function will
  * lookup for "opts.lookupPackageName"
  */
-export async function findDevDependency(opts: { lookupPackageName: string }) {
+export async function findDevDependency(opts: {
+  path?: string;
+  lookupPackageName: string;
+}) {
   const lookupPackageName = opts.lookupPackageName;
 
   return await lookup({
-    path: moduleRootDirectory(),
+    path: opts.path ?? moduleRootDirectory(),
     lookupPackageName,
   });
 }
